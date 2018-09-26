930e078a962627b93d092c4cbb53d0a124af3871
Igor Kucherenko <extern.kucherenko_igor@allianz.com>
2018-09-26 12:12:45 +0300

    Рефакторинг

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
 const CURRENT_VERSION = '0.0.1';
 const USER_CMD = process.argv[2];
 
-const Commands = [
+const commands = [
   {
     input: '--help',
     description: 'печатает этот текст',
     action() {
-      for (let cmd of Commands) {
-        console.log(`${cmd.input} - ${cmd.description}`);
-      }
+      commands.forEach((cmd) => console.log(`${cmd.input} - ${cmd.description}`));
     },
   },
   {
     input: '--version',
     description: 'печатает версию приложения',
     action() {
-      console.log(`v${CURRENT_VERSION}`)
+      console.log(`v${CURRENT_VERSION}`);
     },
   },
 ];
 
 const startApp = function () {
   if (!USER_CMD) {
-    const noUserCmdMessage = 'Привет, пользователь!\n' +
-      'Эта программа будет запускать сервер «Кексобукинг».\nАвтор: Кучеренко Игорь.';
+    const noUserCmdMessage = `Привет, пользователь!
+Эта программа будет запускать сервер «Кексобукинг».
+Автор: Кучеренко Игорь`;
 
     console.log(noUserCmdMessage);
     return;
   }
 
-  for (let cmd of Commands) {
+  for (const cmd of commands) {
     if (cmd.input === USER_CMD) {
       cmd.action();
       return;
     }
   }
 
-  const unknownCmdMessage = 'Неизвестная команда "' + USER_CMD + '".\n' +
-    'Чтобы прочитать правила использования приложения, наберите "--help"';
+  const unknownCmdMessage = `Неизвестная команда "${USER_CMD}"
+Чтобы прочитать правила использования приложения, наберите "--help"`;
 
   console.error(unknownCmdMessage);
   process.exit(1);
