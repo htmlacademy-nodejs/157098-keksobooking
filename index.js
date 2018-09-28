d35edc89dfd0636268a2430eb6fe2be8f402a5ea
Igor Kucherenko <hotncold1989@gmail.com>
2018-09-28 18:10:38 +0300

    Added logic to make work with current commands

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 'use strict';
 
-const USER_CMD = process.argv[2];
-
 const {commands} = require('./src/commands');
 
-commands.find((cmd) => cmd.input === USER_CMD).execute();
+const USER_CMD = process.argv[2];
+
+function reactOnUnknownCmd() {
+  const helpCmd = commands.find((cmd) => cmd.input === '--help');
+
+  console.error(`Unknown command! The list of possible commands is below:`);
+  helpCmd.execute();
+  process.exit(1);
+}
+
+function startApp(userCmd) {
+  if (!userCmd) {
+    console.log(`Hello, user!
+this program starts Keksobooking server.
+Author: Igor Kucherenko`);
+
+    return;
+  }
+
+  const currentCmd = commands.find((cmd) => userCmd === cmd.input);
+
+  if (currentCmd) {
+    currentCmd.execute();
+  } else {
+    reactOnUnknownCmd();
+  }
+}
+
+startApp(USER_CMD);
