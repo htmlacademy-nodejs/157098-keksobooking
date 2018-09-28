597e5f213e8b58fccb978bafd34f45e2a431646b
Igor Kucherenko <hotncold1989@gmail.com>
2018-09-28 18:09:59 +0300

    Reworked logic of <help> cmd

--- src/help.js
+++ src/help.js
@@ -1,15 +1,21 @@
 'use strict';
 
-const {commands} = require(`./commands`);
+const versionCmd = require(`./version`);
+const authorCmd = require(`./author`);
+const descriptionCmd = require(`./description`);
+const licenseCmd = require(`./license`);
+
+const describedCommands = [
+  versionCmd,
+  authorCmd,
+  descriptionCmd,
+  licenseCmd,
+];
 
 module.exports = {
   input: `--help`,
   description: `Shows possible commands`,
   execute() {
-    commands.forEach((cmd) => {
-      if (cmd.hasOwnProperty('input')) {
-        console.log(`${cmd.input} - ${cmd.description}`);
-      }
-    })
+    describedCommands.forEach((cmd) => console.log(`${cmd.input} - ${cmd.description}`));
   }
 };
