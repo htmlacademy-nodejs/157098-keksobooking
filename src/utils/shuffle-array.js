ddc595ddc3e78d174e110715a666d7ef639ded20
Igor Kucherenko <hotncold1989@gmail.com>
2018-10-05 00:27:45 +0300

    Добавил функцию <shuffleArray>

--- src/utils/shuffle-array.js
+++ src/utils/shuffle-array.js
@@ -1,8 +1,21 @@
 'use strict';
 
+const {getRandomItem} = require(`./get-random-item`);
+
 const shuffleArray = (arr) => {
-  return arr;
+  const arrClone = [...arr];
+  const shuffledArr = [];
+
+  while (arrClone.length) {
+    const randomItemIdx = arrClone.indexOf(getRandomItem(arrClone));
+    const splicedItem = arrClone.splice(randomItemIdx, 1);
+
+    shuffledArr.push(...splicedItem);
+  }
+
+  return shuffledArr;
 };
+
 
 module.exports = {
   shuffleArray,
