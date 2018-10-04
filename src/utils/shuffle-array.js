ad009d8736e3c33acdfe00fd41b47f117594242b
Igor Kucherenko <extern.kucherenko_igor@allianz.com>
2018-10-04 19:52:43 +0300

    Добавил тесты для <shuffleArray>

--- src/utils/shuffle-array.js
+++ src/utils/shuffle-array.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const shuffleArray = (arr) => {
-
+  return arr;
 };
 
 module.exports = {
--- test/shuffle-array.test.js
+++ test/shuffle-array.test.js
@@ -2,3 +2,18 @@
 
 const assert = require(`assert`);
 const {shuffleArray} = require(`../src/utils/shuffle-array`);
+
+describe(`Shuffle array`, () => {
+  it(`Result should have the same length as passed array`, () => {
+    const testData = [1, 2, 3];
+    assert.equal(shuffleArray(testData).length, testData.length);
+  });
+
+  it(`Firing of the function should not modify passed array`, () => {
+    const testData = [1, 2, 3];
+    const testDataSnapshot = [...testData];
+    shuffleArray(testData);
+
+    assert.equal(testData, testDataSnapshot);
+  });
+});
