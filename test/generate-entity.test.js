5f9a4423be83ca82672c0bc4434ac7259e4650d1
Igor Kucherenko <hotncold1989@gmail.com>
2018-10-07 13:48:03 +0300

    Начал писать функцию генерации данных

--- test/generate-entity.test.js
+++ test/generate-entity.test.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const assert = require(`assert`);
+const {generateEntity} = require(`../src/generate-entity`);
+const {isObject} = require(`../src/utils/is-object`);
+
+describe(`Generate entity`, () => {
+  it(`Function should return an object`, () => {
+    assert.equal(isObject(generateEntity()), true);
+  });
+});
