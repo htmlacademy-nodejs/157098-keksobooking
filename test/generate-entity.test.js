7ae3fec2098672010c448a8ecb46150fb45b279a
Igor Kucherenko <hotncold1989@gmail.com>
2018-10-07 21:28:44 +0300

    Новые тесты

--- test/generate-entity.test.js
+++ test/generate-entity.test.js
@@ -2,6 +2,7 @@
 
 const assert = require(`assert`);
 const {generateEntity} = require(`../src/generate-entity`);
+const {mockedEntityData} = require(`../src/generate-entity`);
 const {isObject} = require(`../src/utils/is-object`);
 
 describe(`Generate entity`, () => {
@@ -27,9 +28,59 @@ describe(`Generate entity`, () => {
 
   describe(`Avatar property`, () => {
     const property = `avatar`;
+    const avatar = entity.author.avatar;
+    const baseUrl = `${mockedEntityData.author.avatar}/`;
 
-    it(`Property "${property}" should be a property of "author"`, () => {
+    it(`Should be a property of "author"`, () => {
       assert.ok(entity.author.hasOwnProperty(property));
+    });
+
+    it(`Should be string type`, () => {
+      assert.equal(typeof avatar, `string`);
+    });
+
+    it(`Should have correct url`, () => {
+      assert.notEqual(avatar.indexOf(baseUrl), -1);
+      assert.equal(avatar.indexOf(baseUrl), 0);
+      assert.ok(baseUrl.length < avatar.length);
+    });
+  });
+
+  describe(`Offer property`, () => {
+    const property = `offer`;
+
+    it(`Should be a property of entity`, () => {
+      assert.ok(entity.hasOwnProperty(`${property}`));
+    });
+
+    it(`Should be object type`, () => {
+      assert.ok(isObject(entity[property]));
+    });
+  });
+
+  describe(`Title property`, () => {
+    const property = `title`;
+    const title = entity.offer.title;
+
+    it(`Should be a property of "offer"`, () => {
+      assert.ok(entity.offer.hasOwnProperty(property));
+    });
+
+    it(`Should have string type`, () => {
+      assert.equal(typeof title, `string`);
+    });
+
+    it(`Should exist in provided mocked data`, () => {
+      assert.notEqual(mockedEntityData.offer.titles.indexOf(title), -1);
+    });
+  });
+
+  describe(`Address property`, () => {
+    const address = entity.offer.address;
+    const location = entity.location;
+
+    it(`Should have correct value`, () => {
+      assert.equal(address, `${location.x}, ${location.y}`);
     });
   });
 });
