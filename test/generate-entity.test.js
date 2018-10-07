99a844c15d051e3f65154b48a95cbfaecc427dff
Igor Kucherenko <hotncold1989@gmail.com>
2018-10-07 17:03:24 +0300

    Добавил тесты для генерации данных

--- test/generate-entity.test.js
+++ test/generate-entity.test.js
@@ -8,7 +8,31 @@ describe(`Generate entity`, () => {
   console.log(`TEST`, generateEntity());
   console.log(`TEST2`, generateEntity());
 
-  it(`Function should return an object`, () => {
-    assert.ok(isObject(generateEntity()));
+  const entity = generateEntity();
+
+  describe(`Entity`, () => {
+    it(`Function should return an object`, () => {
+      assert.ok(isObject(entity));
+    });
+  });
+
+  describe(`Author property`, () => {
+    const property = `author`;
+
+    it(`Property "${property}" should be a property of entity`, () => {
+      assert.ok(entity.hasOwnProperty(`${property}`));
+    });
+
+    it(`Type of "${property}" property should be object`, () => {
+      assert.ok(isObject(entity[property]));
+    });
+  });
+
+  describe(`Avatar property`, () => {
+    const property = `avatar`;
+
+    it(`Property "${property}" should be a property of "author"`, () => {
+      assert.ok(entity.author.hasOwnProperty(property));
+    });
   });
 });
